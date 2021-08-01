c49af7312da0da17d4edfb8fffb4a2b1a2e142ce
mrileyowens <m.riley.owens@gmail.com>
2021-08-01 13:33:44 -0400

    Added authorship and description

--- flatten.py
+++ flatten.py
@@ -1,18 +1,24 @@
+# Author: Riley Owens (GitHub: mrileyowens)
+
+# This file creates a moment-one map of
+# 13CO emission data of Cygnus X from the
+# moment-zero 13CO data.
+
 from spectral_cube import SpectralCube
 import astropy.units as u
 
-#Creating spectral cube from 13CO emission datacube
+# Creating spectral cube from 13CO emission datacube
 datacubeFile='C:/Users/15136/OneDrive - University of Cincinnati/Documents/Research/WVU REU/Data/13CO_cube_CygX.fits'
 cube=SpectralCube.read(datacubeFile)
 
-#Assigning spectral unit to cube
+# Assigning spectral unit to cube
 cube=cube.with_spectral_unit(u.km/u.s,velocity_convention='radio')
 
-#Selecting a subset of the cube
+# Selecting a subset of the cube
 cubeSub=cube.spectral_slab(-20.0*u.km/u.s,20.0*u.km/u.s)
 
-#Creating moment-one map from subcube
+# Creating moment-one map from subcube
 cubeSubM1=cubeSub.moment(order=1)
 
-#Saving moment-one map
+# Saving moment-one map
 cubeSubM1.write('13CO_cube_CygX_m1_-20_20.fits',overwrite=True)
